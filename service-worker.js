0061ab7fedab42e173d7c121d3b2111d5ede8f52
Rafael <rafa.java.quevedo@gmail.com>
2018-05-21 16:09:24 -0300

    teste

--- service-worker.js
+++ service-worker.js
@@ -5,19 +5,19 @@ var filesToCache = [
    '/index.html',
    '/scripts/app.js',
    '/styles/inline.css',
-   '/images/clear.png',
-   '/images/cloudy-scattered-showers.png',
-   '/images/cloudy.png',
-   '/images/fog.png',
-   '/images/ic_add_white_24px.svg',
-   '/images/ic_refresh_white_24px.svg',
-   '/images/partly-cloudy.png',
-   '/images/rain.png',
-   '/images/scattered-showers.png',
-   '/images/sleet.png',
-   '/images/snow.png',
-   '/images/thunderstorm.png',
-   '/images/wind.png'
+   'images/clear.png',
+   'images/cloudy-scattered-showers.png',
+   'images/fog.png',
+   'images/cloudy.png',
+   'images/ic_refresh_white_24px.svg',
+   'images/ic_add_white_24px.svg',
+   'images/partly-cloudy.png',
+   'images/rain.png',
+   'images/scattered-showers.png',
+   'images/sleet.png',
+   'images/snow.png',
+   'images/thunderstorm.png',
+   'images/wind.png'
  ];
 
 
